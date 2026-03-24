e233b4eab7228030f9c2d5d910a8511b1d3e37f7
Jai Kumar <jai.kmr.del@gmail.com>
2026-03-24 17:52:44 -0600

    Updated Floating Point

--- Floating Point Number.py
+++ Floating Point Number.py
@@ -1,13 +1,13 @@
-def check_float(N):
-    check = list(N)
-    if (check[0] == '+' or check[0] == '-') and '.' in check:
+def is_float(N):
+    try:
+        float(N)
         return True
-    else:
+    except ValueError:
         return False
-
+    
 if __name__ == '__main__':
     T = int(input())
     if 10>T>0:
         for i in range(T):
             N = input()
-            print(check_float(N))
+            print(is_float(N))
